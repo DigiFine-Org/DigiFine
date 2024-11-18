cee398eefd435f51d7c4742c3d7acec715c87b09
Risafamtii <risafaimtiyas@gmail.com>
2024-11-18 11:58:40 +0530

    index file first commit

---
+++ dashboard/admin/index.php
@@ -0,0 +1,12 @@
+<?php
+$pageConfig = [
+    'title' => 'Ad',
+    'styles' => ["../dashboard.css"],
+    'scripts' => ["../dashboard.js"],
+    'authRequired' => true
+];
+
+include_once "../../includes/header.php";
+require_once "../../db/connect.php";
+
+?>
