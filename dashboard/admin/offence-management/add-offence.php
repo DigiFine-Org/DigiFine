02c77c4195be9da154fca6a01e9b1a39df96420c
Risafamtii <risafaimtiyas@gmail.com>
2024-11-18 12:12:15 +0530

    add offence db and the header

---
+++ dashboard/admin/offence-management/add-offence.php
@@ -0,0 +1,12 @@
+<?php
+$pageConfig = [
+    'title' => 'Add Offence',
+    'styles' => ["../../dashboard.css"],
+    'scripts' => ["../../dashboard.js"],
+    'authRequired' => true
+];
+
+require_once "../../../db/connect.php";
+include_once "../../../includes/header.php";
+
+?>
