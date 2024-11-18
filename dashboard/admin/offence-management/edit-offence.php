85b6ee20162ad8088e5d242a46c8471ceadc1fd1
Risafamtii <risafaimtiyas@gmail.com>
2024-11-18 12:39:15 +0530

    adding main form fields

--- dashboard/admin/offence-management/edit-offence.php
+++ dashboard/admin/offence-management/edit-offence.php
@@ -41,7 +41,30 @@ if(!$offence) {
             <div class="container">
                 <h1>Edit Offence</h1>
                 <form action="edit-offence-process.php" method="post">
-                ...
+                ...<div class="field">
+                        <label for="">Offence Number</label>
+                        <input type="text" class="input" placeholder="001" name="offence_number" value="<?= htmlspecialchars($offence['offence_number']) ?>" readonly required>
+                    </div>
+                    <div class="field">
+                        <label for="">Offence Description (Sinhala):</label>
+                        <input type="text" class="input" placeholder="හඳුනාගැනීමේ තහඩු" name="description_sinhala" value="<?= htmlspecialchars($offence['description_sinhala']) ?>"  required>
+                    </div>
+                    <div class="field">
+                        <label for="">Offence Description (Tamil):</label>
+                        <input type="text" class="input" placeholder="அடையாளங்காணல் தகடுகள்" name="description_tamil" value="<?= htmlspecialchars($offence['description_tamil']) ?>" required>
+                    </div>
+                    <div class="field">
+                        <label for="">Offence Description (English):</label>
+                        <input type="text" class="input" placeholder="Identification Plates" name="description_english" value="<?= htmlspecialchars($offence['description_english']) ?>" required>
+                    </div>
+                    <div class="field">
+                        <label for="">Points Deducted:</label>
+                        <input type="text" class="input" placeholder="4" name="points_deducted" value="<?= htmlspecialchars($offence['points_deducted']) ?>" required>
+                    </div>
+                    <div class="field">
+                        <label for="">Fine Amount(RS):</label>
+                        <input type="text" class="input" placeholder="1000.00" name="fine" value="<?= htmlspecialchars($offence['fine']) ?>" required>
+                    </div>
             </div>
         </div>
     </div>
