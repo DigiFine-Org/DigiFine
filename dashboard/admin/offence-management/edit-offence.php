e4b99fdab9b173b00a86e668264acfdaa0e9bda6
Risafamtii <risafaimtiyas@gmail.com>
2024-11-18 12:35:10 +0530

    set up page configuration with title, styles, and scripts for edit-offence

---
+++ dashboard/admin/offence-management/edit-offence.php
@@ -0,0 +1,15 @@
+<?php
+$pageConfig = [
+    'title' => 'Add Offence',
+    'styles' => ["../../dashboard.css"],
+    'scripts' => ["../../dashboard.js"],
+    'authRequired' => true
+];
+
+
+
+
+
+
+
+?>
