729729e88a2666b539495ce35e92a5854561fe2f
Risafamtii <risafaimtiyas@gmail.com>
2024-11-18 12:49:49 +0530

    dynamically render offence data into table rows

--- dashboard/admin/offence-management/index.php
+++ dashboard/admin/offence-management/index.php
@@ -66,6 +66,28 @@ include_once "../../../includes/header.php";
                             <th>Action</th>
                         </tr>
                     </thead>
+                    <tbody>
+                        <?php foreach ($offences as $offence): ?>
+                            <tr>
+                                <td><?php echo htmlspecialchars($offence['offence_number']); ?></td>
+                                <td><?php echo htmlspecialchars($offence['description_sinhala']); ?></td>
+                                <td><?php echo htmlspecialchars($offence['description_tamil']); ?></td>
+                                <td><?php echo htmlspecialchars($offence['description_english']); ?></td>
+                                <td><?php echo htmlspecialchars($offence['points_deducted']); ?></td>
+                                <td><?php echo "Rs. " . number_format($offence['fine'], 2); ?></td>
+                                <td>
+                                    <a href="edit-offence.php?offence_number=<?php echo urlencode($offence['offence_number']); ?>"
+                                        class="btn marginbottom">Edit</a>
+                                    <form action="delete-offence-process.php" method="POST" style="display:inline;">
+                                        <input type="hidden" name="offence_number"
+                                            value="<?php echo htmlspecialchars($offence['offence_number']); ?>">
+                                        <button type="submit" class="deletebtn">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+
 
 
 
