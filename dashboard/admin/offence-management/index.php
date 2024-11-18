e4c593188d6528d3759d1219dc8f1187a6d726cc
Risafamtii <risafaimtiyas@gmail.com>
2024-11-18 12:44:42 +0530

    create main layout with navbar and sidebar inclusion

--- dashboard/admin/offence-management/index.php
+++ dashboard/admin/offence-management/index.php
@@ -40,6 +40,15 @@ try {
 
 include_once "../../../includes/header.php";
 
+<main>
+    <?php include_once "../../includes/navbar.php" ?>
+    <div class="dashboard-layout">
+        <?php include_once "../../includes/sidebar.php" ?>
+        <div class="content">
+            ...
+        </div>
+    </div>
+</main>
 
 
 
