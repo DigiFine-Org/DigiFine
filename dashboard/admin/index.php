22434462bbf21d2c9d8cd88f011c2afce32f766c
Risafamtii <risafaimtiyas@gmail.com>
2024-11-18 12:02:47 +0530

    index file <main> commit

--- dashboard/admin/index.php
+++ dashboard/admin/index.php
@@ -10,3 +10,16 @@ include_once "../../includes/header.php";
 require_once "../../db/connect.php";
 
 ?>
+
+<main>
+    <?php include_once "../includes/navbar.php" ?>
+
+    <div class="dashboard-layout">
+        <?php include_once "../includes/sidebar.php" ?>
+        <div class="content">
+            <div class="container x-large no-border">
+
+            </div>
+        </div>
+    </div>
+</main>
