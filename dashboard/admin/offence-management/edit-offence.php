0c3857c2869d4931b6ac233e92363922a4805b21
Risafamtii <risafaimtiyas@gmail.com>
2024-11-18 12:36:23 +0530

    add logic to fetch offence data from the database by offence number

--- dashboard/admin/offence-management/edit-offence.php
+++ dashboard/admin/offence-management/edit-offence.php
@@ -10,7 +10,26 @@ require_once "../../../db/connect.php";
 include_once "../../../includes/header.php";
 
 
+$offence = null;
+if (isset($_GET['offence_number']) && is_numeric($_GET['offence_number'])) {
+    $offence_number = $_GET['offence_number'];
 
+    $stmt = $conn->prepare("SELECT *FROM offences WHERE offence_number = ?");
+    if($stmt) {
+        $stmt->bind_param("i", $offence_number);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result && $result->num_rows > 0) {
+            $offence = $result->fetch_assoc();
+        }
+        $stmt->close();
+    }
+}
+
+if(!$offence) {
+    echo "<script>alert('Offence not found!'); window.location.href = '/digifine/dashboard/admin/offence-management/index.php';</script>";
+    exit();
+}
 
 
 
