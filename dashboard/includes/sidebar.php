ecc7b425e9c2679d40102c6fa018c600184da3d5
Imalsha Jathunarachchi <imalsha.contact@gmail.com>
2024-11-24 12:22:47 +0530

    sidebar authentication and navigation updated

--- dashboard/includes/sidebar.php
+++ dashboard/includes/sidebar.php
@@ -10,28 +10,31 @@ function renderLink(string $title, string $link)
 }
 ?>
 <div class="sidebar">
-    <?php renderLink("Home", "/digifine/dashboard/index.php") ?>
+    <?php if ($sidebar_user['role'] === 'oic'): ?>
+        <!-- oic links -->
+        <?php renderLink("Home", "/digifine/dashboard/oic/index.php") ?>
+        <?php renderLink("Fines", "/digifine/dashboard/oic/fine-management/index.php") ?>
+        <?php renderLink("Duty Submissions", "/digifine/dashboard/oic/duty-submissions/index.php") ?>
+        <?php renderLink("Station Officers", "/digifine/dashboard/oic/officer-management/index.php") ?>
+    <?php endif ?>
+
     <?php if ($sidebar_user['role'] === 'officer'): ?>
-        <?php if ($sidebar_user['is_oic'] === "1"): ?>
-            <!-- oic links -->
-            <?php renderLink("Fines", "/digifine/dashboard/oic/fine-management/index.php") ?>
-            <?php renderLink("Duty Submissions", "/digifine/dashboard/oic/duty-submissions/index.php") ?>
-            <?php renderLink("Station Officers", "/digifine/dashboard/oic/officer-management/index.php") ?>
-        <?php else: ?>
-            <?php renderLink("Check Vehicle Details", "/digifine/dashboard/p-officer/check-vehicle-details/index.php") ?>
-            <?php renderLink("Generate E-Ticket", "/digifine/dashboard/p-officer/generate-e-ticket/index.php") ?>
-            <?php renderLink("Verify Driver Details", "/digifine/dashboard/p-officer/verify-driver-details/index.php") ?>
-            <?php renderLink("Submit Duty", "/digifine/dashboard/p-officer/submit-duty/index.php") ?>
-        <?php endif; ?>
+        <?php renderLink("Home", "/digifine/dashboard/officer/index.php") ?>
+        <?php renderLink("Check Vehicle Details", "/digifine/dashboard/officer/check-vehicle-details/index.php") ?>
+        <?php renderLink("Generate E-Ticket", "/digifine/dashboard/officer/generate-e-ticket/index.php") ?>
+        <?php renderLink("Verify Driver Details", "/digifine/dashboard/officer/verify-driver-details/index.php") ?>
+        <?php renderLink("Submit Duty", "/digifine/dashboard/officer/submit-duty/index.php") ?>
     <?php endif ?>
 
     <?php if ($sidebar_user['role'] === 'driver'): ?>
+        <?php renderLink("Home", "/digifine/dashboard/driver/index.php") ?>
         <?php renderLink("My Fines", "/digifine/dashboard/driver/my-fines/index.php") ?>
         <?php renderLink("Gov-Offence List", "/digifine/dashboard/gov-fine-list/index.php") ?>
         <?php renderLink("Payments", "/digifine/dashboard/driver/payments/index.php") ?>
     <?php endif ?>
 
     <?php if ($sidebar_user['role'] === 'admin'): ?>
+        <?php renderLink("Home", "/digifine/dashboard/admin/index.php") ?>
         <?php renderLink("Assign OIC", "/digifine/dashboard/admin/assign-oic/index.php") ?>
         <?php renderLink("Offence Management", "/digifine/dashboard/admin/Offence-management/index.php") ?>
         <?php renderLink("Driver Management", "/digifine/dashboard/admin/driver-management/index.php") ?>
