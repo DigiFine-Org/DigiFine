7ae85b82594578dfb80682b975335a192c7f728a
Pubuditha2001 <pubuditha2001@gmail.com>
2024-11-27 01:40:56 +0530

    new nav bar

--- landingPage/try 3/index.php
+++ landingPage/try 3/index.php
@@ -9,20 +9,21 @@
 </head>
 
 <body>
-    <header>
+    <header class="overlay-header">
         <div class="header-container">
             <img src="../../assets/logo-white.svg" alt="Logo" class="logo">
+            <button class="hamburger" aria-label="Toggle navigation">&#9776;</button>
+            <nav>
+                <ul class="nav-links">
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Reports</a></li>
+                    <li><a href="#">Contacts</a></li>
+                    <li><a href="../../login/index.php">Login</a></li>
+                    <li><a href="../../signup/index.php">Register</a></li>
+                </ul>
+            </nav>
         </div>
-        <nav>
-            <ul>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Reports</a></li>
-                <li><a href="#">Contacts</a></li>
-                <li><a href="../../login/index.php">Login</a></li>
-                <li><a href="../../signup/index.php">Register</a></li>
-            </ul>
-        </nav>
     </header>
 
     <section class="hero">
@@ -50,14 +51,14 @@
         <div class="feature">
             <a href="#" style="text-decoration: none; color: inherit;">
                 <div class="icon">&#128663;</div>
-                <h3>Police Patrol</h3>
+                <h3>Pay Fines Online</h3>
                 <p>Liquam rhoncus, libero non congue ultricies. Lorem ipsum dolor sit amet.</p>
             </a>
         </div>
         <div class="feature">
             <a href="#" style="text-decoration: none; color: inherit;">
                 <div class="icon">&#128663;</div>
-                <h3>Police Patrol</h3>
+                <h3>Traffic Offence List</h3>
                 <p>Liquam rhoncus, libero non congue ultricies. Lorem ipsum dolor sit amet.</p>
             </a>
         </div>
