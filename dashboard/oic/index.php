0db75632b0ca81915e4605151066de950d9bf7f9
Risafamtii <risafaimtiyas@gmail.com>
2024-11-27 18:39:34 +0530

    syntax

--- dashboard/oic/index.php
+++ dashboard/oic/index.php
@@ -53,43 +53,43 @@ $conn->close();
             <h2>Welcome OIC <?= htmlspecialchars($oic['fname'] . ' ' . $oic['lname']) ?>!</h2>
             <p>Police Station: <?= htmlspecialchars($oic['police_station_name']) ?></p>
             <div class="insights-bar">
-    <div class="inner-tile">
-        <div class="icon" style="background-color: #FFEFB4;">
-            <span class="material-icons" style="font-size: 40px; color: #333;">directions_car</span>
+            <div class="inner-tile">
+                <div class="icon" style="background-color: #FFEFB4;">
+                    <span class="material-icons" style="font-size: 40px; color: #333;">directions_car</span>
+                </div>
+                <div class="info">
+                    <p>Drivers</p>
+                    <h3>248</h3>
+                </div>
+            </div>
+            <div class="inner-tile">
+                <div class="icon" style="background-color: #CDE4FF;">
+                    <span class="material-icons" style="font-size: 40px; color: #333;">groups</span>
+                </div>
+                <div class="info">
+                    <p>Police Officers</p>
+                    <h3>56</h3>
+                </div>
+            </div>
+            <div class="inner-tile">
+                <div class="icon" style="background-color: #F8C8D8;">
+                    <span class="material-icons" style="font-size: 40px; color: #333;">local_shipping</span>
+                </div>
+                <div class="info">
+                    <p>Stolen Vehicles</p>
+                    <h3>15</h3>
+                </div>
+            </div>
+            <div class="inner-tile">
+                <div class="icon" style="background-color: #D5F2EA;">
+                    <span class="material-icons" style="font-size: 40px; color: #333;">monetization_on</span>
+                </div>
+                <div class="info">
+                    <p>Total Fines</p>
+                    <h3>Rs 164,920</h3>
+                </div>
+            </div>
         </div>
-        <div class="info">
-            <p>Drivers</p>
-            <h3>248</h3>
-        </div>
-    </div>
-    <div class="inner-tile">
-        <div class="icon" style="background-color: #CDE4FF;">
-            <span class="material-icons" style="font-size: 40px; color: #333;">groups</span>
-        </div>
-        <div class="info">
-            <p>Police Officers</p>
-            <h3>56</h3>
-        </div>
-    </div>
-    <div class="inner-tile">
-        <div class="icon" style="background-color: #F8C8D8;">
-            <span class="material-icons" style="font-size: 40px; color: #333;">local_shipping</span>
-        </div>
-        <div class="info">
-            <p>Stolen Vehicles</p>
-            <h3>15</h3>
-        </div>
-    </div>
-    <div class="inner-tile">
-        <div class="icon" style="background-color: #D5F2EA;">
-            <span class="material-icons" style="font-size: 40px; color: #333;">monetization_on</span>
-        </div>
-        <div class="info">
-            <p>Total Fines</p>
-            <h3>Rs 164,920</h3>
-        </div>
-    </div>
-</div>
 
           
         </div>
