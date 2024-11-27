38fd6423a10ac8f0ea316ca8105153fb11f5b76e
Imalsha Jathunarachchi <imalsha.contact@gmail.com>
2024-11-27 15:44:21 +0530

    driver dashboard update

--- dashboard/driver/index.php
+++ dashboard/driver/index.php
@@ -1,16 +1,46 @@
 <?php
 $pageConfig = [
     'title' => 'Payments',
-    'styles' => ["../dashboard.css"],
+    'styles' => ["../dashboard.css", "./driver-dashboard.css"],
     'scripts' => ["../dashboard.js"],
     'authRequired' => true
 ];
 
 include_once "../../includes/header.php";
+require_once "../../db/connect.php";
+
 
 if ($_SESSION['user']['role'] !== 'driver') {
     die("unauthorized user!");
 }
+
+$driverId = $_SESSION['user']['id'] ?? null;
+
+if (!$driverId) {
+    die("Unauthorized access.");
+}
+
+// fetch driver's nameS
+$sql = "SELECT fname, lname FROM drivers WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $driverId);
+
+if (!$stmt->execute()) {
+    die("Error fetching officer details " . $stmt->error);
+}
+
+$result = $stmt->get_result();
+if ($result->num_rows === 0) {
+    die("Driver not found.");
+}
+
+$driver = $result->fetch_assoc();
+$stmt->close();
+$conn->close();
+
+
+
+
 ?>
 
 <main>
@@ -18,12 +48,28 @@ if ($_SESSION['user']['role'] !== 'driver') {
     <div class="dashboard-layout">
         <?php include_once "../includes/sidebar.php" ?>
         <div class="content">
-            <div class="content">
-                <div class="home-grid">
-                    
+            <h2>Welcome Driver <?= htmlspecialchars($driver['fname'] . ' ' . $driver['lname']) ?>!</h2>
+            <p>A responsible driver is a true road hero.</p>
+            <div class="navigation-tile-grid" style="margin-top: 40px;">
+                <div class="tile emergency-services">
+                    <span>Emergency Services</span>
+                </div>
+                <div class="tile tips-drivers">
+                    <span>Tips For Drivers</span>
+                </div>
+                <div class="tile traffic-signs">
+                    <span>Traffic Signs</span>
+                </div>
+                <div class="tile remaining-points">
+                    <span>Remaining Points</span>
+                </div>
+                <div class="tile tell-igp">
+                    <span>Tell IGP</span>
+                </div>
+                <div class="tile police-stations">
+                    <span>Police Stations</span>
                 </div>
             </div>
-        </div>
 </main>
 
 <?php include_once "../../includes/footer.php" ?>
