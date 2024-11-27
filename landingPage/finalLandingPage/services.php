c1166048203a81bd7ea6f98e534a3c6eaf15c7b9
Pubuditha2001 <pubuditha2001@gmail.com>
2024-11-27 17:24:30 +0530

    landing other pages to liseka

---
+++ landingPage/finalLandingPage/services.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Digi-Fine | Services</title>
+    <link rel="stylesheet" href="styles.css">
+    <!-- <link rel="stylesheet" href="../../styles/components.css"> -->
+</head>
+
+<body>
+    <?php
+    include_once 'navbar.php';
+    ?>
+
+    <main>
+        <section class="chief-message">
+            <div class="message-content">
+                <h1>services</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quaerat magni ratione distinctio aperiam eveniet ab reprehenderit! Quae sed ipsum quo, officiis maiores odio exercitationem, ea consequatur illo quisquam id!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus dolore alias provident quae sit quod. Corrupti explicabo nostrum accusantium nemo libero saepe quia, quidem molestiae necessitatibus totam porro eveniet voluptate?
+                </p>
+                <a href="../../signup/index.php" class="btn-primary">Get Started</a>
+            </div>
+        </section>
+    </main>
+
+    <?php
+    include_once 'footer.php';
+    ?>
+
+</body>
+
+</html>
