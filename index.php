471d241c29154758ca9077bbbaeeb73ed377ca11
Imalsha Jathunarachchi <imalsha.contact@gmail.com>
2025-04-23 21:49:27 +0530

    fix undefined inde.php direct

--- index.php
+++ index.php
@@ -1,11 +1,39 @@
+// include_once "config.php";
+
+// session_start();
+
+// if (isset($_SESSION['user'])) {
+// header("Location: /digifine/dashboard/index.php");
+// } else {
+// header("Location: /digifine/landingPage/index.php");
+// }
+
+
 <?php
-
 include_once "config.php";
-
 session_start();
 
 if (isset($_SESSION['user'])) {
-    header("Location: /digifine/dashboard/index.php");
+    $role = $_SESSION['user']['role']; // Assuming role is stored in the user session data
+
+    switch ($role) {
+        case 'admin':
+            header("Location: /digifine/dashboard/admin/index.php");
+            break;
+        case 'oic':
+            header("Location: /digifine/dashboard/oic/index.php");
+            break;
+        case 'driver':
+            header("Location: /digifine/dashboard/driver/index.php");
+            break;
+        case 'officer':
+            header("Location: /digifine/dashboard/officer/index.php");
+            break;
+        default:
+            header("Location: /digifine/dashboard/index.php");
+            break;
+    }
 } else {
     header("Location: /digifine/landingPage/index.php");
 }
+exit();
